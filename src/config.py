205283de934af2ddc1be00f5e05466d1977781bd
Aditya-0156 <aditya21374@iiitd.ac.in>
2026-01-11 22:11:52 +0530

    config file changed with gemini settings and name

--- src/config.py
+++ src/config.py
@@ -4,17 +4,16 @@ Uses ppydantic-settings for type-safe configuration with environment vairables
 """
 
 from pydantic_settings import BaseSettings, SetttingsConfigDict
-from pathib import Path 
+from pathlib import Path 
 
 class Settings(BaseSettings):
 
     #API KEYS
-
-    openai_api-key: str =""
+    google_api_key: str=""
 
     #Model Configuration 
-    embedding_model: str=""
-    llm_model: str =""
+    embedding_model: str="models/text-embedding-004"
+    llm_model: str ="gemini-2.0-flash-exp"
     chunk_size: int =1000
     chunk_overlap: int =200
 
@@ -24,3 +23,21 @@ class Settings(BaseSettings):
     raw_data_dir: Path= Path("data/raw")
     processed_data_dir: Path =Path("data/processed")
     chroma_persist_directory: Path= Path("data/chroma_db")
+
+    # Vector Store Settings
+
+    collection_name: str ="rag_documents"
+    top_k_results: int =5
+    
+    #Temperature for the model 
+    temperature: float =0.7
+    
+    #Model configuration to load from .env file 
+
+    model_config= SettingsConfigDict(
+        env_file=".env",
+         env_file_encoding="utf-8",
+        case_sensitive=False)
+    
+#Global settings instance
+settings = Settings()
